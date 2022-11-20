4251e92c751272d7293e1a906cf0357f524f8473
Henrry Bourgeot <henrrybrgt@gmail.com>
2022-11-19 21:59:59 -0400

    main

---
+++ datatypes.py
@@ -0,0 +1,35 @@
+''' Tipos de datos en python
+
+En python, existen los siguientes tipos de datos:
+1. Numéricos
+  1. Enteros
+  2. Números complejos
+  3. Flotantes
+2. Diccionarios (objetos en JS)
+3. Booleanos
+4. Sets
+5. Secuencias
+  1. Strings
+  2. Listas
+  3. Tuplas
+'''
+
+# Enteros
+x = 10 
+print(x)
+print(type(x)) # int
+
+# string
+y = "hola mundo"
+print(y)
+print(type(y)) # str
+
+# flotantes
+z = 45.53
+print(z)
+print(type(z)) # float
+
+# booleanos
+w = True # tambien puede ser false, solo que con la F mayuscula
+print(w)
+print(type(w)) # bool
---
+++ variables.py
@@ -0,0 +1,18 @@
+# Hola mundo
+print("Hola Mundo!")
+
+# Uso de variables
+miVariable = 2 # declaracion
+print(miVariable) # uso
+
+miVariable = 10 # asignacion
+print(miVariable)
+
+# Aritmetica con variables
+x = 10
+y = 2 # en python no se necesita indicar el tipo de dato
+z = x + y
+print(z)
+
+# Funcion para saber la direccion de memoria de un valor
+print(id(x))
